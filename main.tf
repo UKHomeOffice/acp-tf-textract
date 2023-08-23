4955d293417cbbdd3afae90137258e267c2bd811
Rob Johnson <robert.johnson@digital.homeoffice.gov.uk>
2023-08-23 16:52:07 +0100

    Update policy attachment name so it is unique. Fix example code snippet. Correct iam user name tag.

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ Module usage:
          iam_user_policy_name = "fake-textract-policy"
          // base64 encoded public gpg key for secret output 
          gpg_key = "LS0t...."
-         environment = "${var.environment}"
+         environment = var.environment
          tags = {
            // any required tags
         }
@@ -23,7 +23,7 @@ resource "aws_iam_user" "textract_iam_user" {
   tags = merge(
     var.tags,
     {
-      "Name" = format("%s-%s", var.environment, var.iam_user_policy_name)
+      "Name" = format("%s-%s", var.environment, var.textract_iam_user)
     },
     {
       "Env" = var.environment
@@ -31,12 +31,13 @@ resource "aws_iam_user" "textract_iam_user" {
   )
 }
 
-resource "aws_iam_policy_attachment" "textract_iam_user_policy_attach" {
-  user       = aws_iam_user.textract_iam_user.name
+resource "aws_iam_policy_attachment" "textract_iam_user_policy_attachment" {
+  name       = format("%s-%s-%s", var.environment, var.textract_iam_user, var.iam_user_policy_name)
+  users      = [aws_iam_user.textract_iam_user.name]
   policy_arn = data.aws_iam_policy.AmazonTextractFullAccess
 }
 
 resource "aws_iam_access_key" "textract_iam_user_access_key" {
-  user = aws_iam_user.textract_iam_user.name
+  user    = aws_iam_user.textract_iam_user.name
   pgp_key = var.gpg_key
 }
